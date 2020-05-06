f76776304e2e8cec722baafb4b55ce6e44c05449
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-05-06 22:41:44 +0600

    create style for header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import styles from './header.module.css';
+import smile from '../../assets/images/smile.png';
 
 export default function Header(props) {
   const { counter, time } = props;
 
   return (
     <header className={styles.Container}>
-      <div>
-        {counter}
+      <div className={styles.Display}>
+        {counter.toString().padStart(3, 0)}
       </div>
 
-      <button type="button">
-        restart
+      <button type="button" className={styles.Restart}>
+        <img src={smile} alt="smile" />
       </button>
 
-      <div>
-        {time}
+      <div className={styles.Display}>
+        {time.toString().padStart(3, 0)}
       </div>
     </header>
   );
