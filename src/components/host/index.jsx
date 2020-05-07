90b4d41a23f0674d0a3423432c9032737bd7d3e7
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-05-07 21:59:34 +0600

    add data generator

--- src/components/host/index.jsx
+++ src/components/host/index.jsx
@@ -3,29 +3,39 @@ import Header from '../header';
 import Field from '../field';
 import styles from './host.module.css';
 
-const data = [
-  [
-    { tip: '4' },
-    {
-      tip: '7',
-      bomb: true,
-    },
-  ],
-  [
-    { tip: '2' },
-    { tip: '9' },
-    {},
-  ],
-];
-
+const maxElements = 10;
 const counter = 5;
 const time = 10;
 
-export default function Host() {
-  return (
-    <div className={styles.Container}>
-      <Header counter={counter} time={time} />
-      <Field data={data} />
-    </div>
-  );
+export default class Host extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    this.generateData();
+  }
+
+  generateData() {
+    const rows = [];
+    for (let i = 0; i < maxElements; i += 1) {
+      const elements = [];
+      for (let j = 0; j < maxElements; j += 1) {
+        elements.push({});
+      }
+      rows.push(elements);
+    }
+    this.setState({ data: rows });
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.Container}>
+        <Header counter={counter} time={time} />
+        <Field data={data} />
+      </div>
+    );
+  }
 }
